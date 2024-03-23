ac36d33e2c8237455de34fd624e7eb22ba168b33
basicinfosecurity <jerowin.santillan@gmail.com>
2024-03-24 01:26:10 +0800

    Add maven script

---
+++ Maven/Get-MavenData.ps1
@@ -0,0 +1,149 @@
+<#
+    .Description
+    Get-MavenData is a script for searching and gathering artifact data including dependencies data from maven central. The data is then saved to csv.
+    .Parameter ArtifactId
+    The name of the artifact (e.g. jackson-databind).
+    .Parameter GroupId
+    The name of the group (e.g. com.fasterxml.jackson.core).
+    .Parameter Version
+    The version number (e.g. 2.15.0-rc1)
+    .Parameter GetDependencies
+    If this paramter is included, the script will retrieve dependencies of each artifact.
+    .Parameter Suppress
+    If this paramter is included, the script will hide console output.
+    .Parameter Proxy
+    Directs the script to use a proxy.
+    .SYNOPSIS
+    Get-MavenData is a script for retrieving artifact data from maven.
+    .EXAMPLE
+    Get-MavenData -ArtifactId jackson-databind -GroupId com.fasterxml.jackson.core
+    .EXAMPLE
+    Get-MavenData -ArtifactId jackson-databind -Version 2.17.0 -GetDependencies
+    .EXAMPLE
+    Get-MavenData -GroupId org.springframework -Suppress
+#>
+function Get-MavenData{
+    param (
+        [Parameter()]
+        [string]$ArtifactId,
+        [string]$GroupId,
+        [string]$Version,
+        [string]$Proxy = "",
+        [switch]$GetDependencies = $false,
+        [switch]$Suppress = $false
+    )
+    begin{
+        $page = 0
+        $pages = 1
+        $params = foreach($param in @("g:$GroupId", "a:$ArtifactId", "v:$Version")){
+            if($param.Length -gt 2){
+                $param
+            }
+        }
+        $offset = 100
+        $query = $params -join " AND "
+        $Body = @{
+            "q" = $query
+            "wt" = "json"
+            "rows" = $offset
+            "core" = "gav"
+            "start" = 0
+        }
+        $searchEndpoint = "https://search.maven.org/solrsearch/select"
+        $dependenciesEndpoint = "https://central.sonatype.com/api/internal/browse/dependencies"
+        if($Suppress){
+            function Write-Host {}
+        }
+    }
+    process{
+        try {
+            Write-Host "[*] Retrieving artifacts."
+            $searchData = [System.Collections.ArrayList]::new()
+            while ($page -lt $pages) {
+                $arguments = @{
+                    UseBasicParsing = $true
+                    Uri = $searchEndpoint
+                    Body = $Body
+                }
+                if($Proxy){ $arguments["Proxy"] = $Proxy}
+                $request = Invoke-RestMethod @arguments
+                $total = $request.response.numFound
+                if($total -eq 0) { throw "No items found." }
+                $pages = [System.Math]::Floor([decimal]($total / $offset)) + 1
+                $page += 1
+                Write-Host "`t[+] Getting $page out of $pages pages."
+                $Body["start"] = $page * $offset
+                foreach($item in $request.response.docs){
+                    $tmp = [PSCustomObject]@{
+                        ArtifactId = $item.id
+                        GroupId = $item.g
+                        Name = $item.a
+                        Version = $item.v
+                    }
+                    $null = $searchData.Add($tmp)
+                }
+            }
+            if($GetDependencies){
+                Write-Host "[*] Retrieving dependencies"
+                $dependencies = [System.Collections.ArrayList]::new()
+                foreach($artifact in $searchData){
+                    $page = 0
+                    $pages = 1
+                    $purl = "pkg:maven/$($artifact.GroupId)/$($artifact.Name)@$($artifact.Version)"
+                    $offset = 20 # This seems to be the maximum, anything else will return HTTP 400
+                    $json = @{
+                        "page" = $page
+                        "purl" = $purl
+                        "searchTerm" = $SearchTerm
+                        "size" = $offset
+                    }
+                    $contentType = "application/json"
+                    $headers = @{
+                        "Content-Type" = $contentType
+                        "Accept" = $contentType
+                    }
+                    Write-Host "[*] $($artifact.ArtifactId)"
+                    while($page -lt $pages){
+                        $arguments = @{
+                            UseBasicParsing = $true
+                            Uri = $dependenciesEndpoint
+                            Body = ($json | ConvertTo-Json)
+                            Method = "Post"
+                            Headers = $headers
+                        }
+                        if($Proxy){ $arguments["Proxy"] = $Proxy}
+                        $response = Invoke-RestMethod @arguments
+                        if($response.totalResultCount -eq 0) {
+                            $null = $dependencies.Add($item)
+                            break
+                        }
+                        $pages = $response.pageCount
+                        $page += 1
+                        Write-Host "`t[+] Getting $page out of $pages pages."
+                        $json["page"] = $page
+                        foreach($item in $response.components){
+                            $item.licenses = $item.licenses -join "|| "
+                            $null = $dependencies.Add($item)
+                        }
+                    }
+                }
+            }
+        }
+        catch {
+            if($Suppress){ Remove-Item -Path function:Write-Host }
+            Write-Host "[-] $($_.Exception.Message)" -ForegroundColor "Red"
+            Write-Host "`t$($PSItem.ScriptStackTrace)" -ForegroundColor "Red"
+        }
+        finally {
+            $timestamp = $(Get-Date -Format ddMMyyyy_hhmmss)
+            if($searchData.Count -gt 0){
+                $csv = "maven_artifacts-$timestamp.csv"
+                $searchData | Export-Csv -NoTypeInformation $csv
+            }
+            if($dependencies.Count -gt 0){
+                $csv = "maven_dependencies-$timestamp.csv"
+                $dependencies | Export-Csv -NoTypeInformation $csv
+            }
+        }
+    }
+}
